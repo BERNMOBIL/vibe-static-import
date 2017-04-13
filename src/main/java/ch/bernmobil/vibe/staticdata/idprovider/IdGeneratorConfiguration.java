5685b4fe12be59fd5b484e6f9a6aab39ea4aabc9
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2017-04-13 12:53:17 +0200

    Add line to route

---
+++ src/main/java/ch/bernmobil/vibe/staticdata/idprovider/IdGeneratorConfiguration.java
@@ -0,0 +1,15 @@
+package ch.bernmobil.vibe.staticdata.idprovider;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IdGeneratorConfiguration {
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public IdGenerator sequentialIdGenerator() {
+        return new IdGenerator();
+    }
+}

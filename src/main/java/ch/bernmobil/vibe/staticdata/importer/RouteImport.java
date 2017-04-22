3b4031d22fef126fb69b0023fc8763469003d725
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2017-04-22 14:45:03 +0200

    Fix line in route

--- src/main/java/ch/bernmobil/vibe/staticdata/importer/RouteImport.java
+++ src/main/java/ch/bernmobil/vibe/staticdata/importer/RouteImport.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 public class RouteImport extends Import<GtfsRoute, Route> {
     private static String[] fieldNames = {"route_id", "agency_id", "route_short_name", "route_long_name", "route_desc", "route_type", "route_url", "route_color", "route_text_color"};
     private static String path = "routes.txt";
-    private static final String insertQuery = "INSERT INTO route (id, type) VALUES(?, ?)";
+    private static final String insertQuery = "INSERT INTO route (id, type, line) VALUES(?, ?, ?)";
 
 
     public RouteImport(DataSource dataSource, String folder) {
@@ -26,6 +26,7 @@ public class RouteImport extends Import<GtfsRoute, Route> {
         public void setValues(Route item, PreparedStatement ps) throws SQLException {
             ps.setLong(1, item.getId());
             ps.setInt(2, item.getType());
+            ps.setString(3, item.getLine());
         }
     }
 }

47dce55a76e06bf7e014e0de478506aaaafce5a3
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2017-04-22 14:45:03 +0200

    Fix line in route
    (cherry picked from commit 3b4031d)

--- src/main/java/ch/bernmobil/vibe/staticdata/importer/RouteImport.java
+++ src/main/java/ch/bernmobil/vibe/staticdata/importer/RouteImport.java
@@ -3,17 +3,15 @@ package ch.bernmobil.vibe.staticdata.importer;
 import ch.bernmobil.vibe.staticdata.entity.Route;
 import ch.bernmobil.vibe.staticdata.fieldsetmapper.RouteFieldSetMapper;
 import ch.bernmobil.vibe.staticdata.gtfsmodel.GtfsRoute;
-import javax.sql.DataSource;
-import org.springframework.batch.item.database.ItemPreparedStatementSetter;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
 
 public class RouteImport extends Import<GtfsRoute, Route> {
     private static String[] fieldNames = {"route_id", "agency_id", "route_short_name", "route_long_name", "route_desc", "route_type", "route_url", "route_color", "route_text_color"};
     private static String path = "routes.txt";
-    private static final String insertQuery = "INSERT INTO route (id, type) VALUES(?, ?)";
+    private static final String insertQuery = "INSERT INTO route (id, type, line) VALUES(?, ?, ?)";
 
 
     public RouteImport(DataSource dataSource, String folder) {
@@ -26,6 +24,7 @@ public class RouteImport extends Import<GtfsRoute, Route> {
         public void setValues(Route item, PreparedStatement ps) throws SQLException {
             ps.setLong(1, item.getId());
             ps.setInt(2, item.getType());
+            ps.setString(3, item.getLine());
         }
     }
 }
